cedaf1a16f8f80ee6162a4977f47b49f1de7948c
De-Dremer <abhinavbagali65@gmail.com>
2025-06-06 17:13:12 +0530

    Prepare for login: backend setup, ignore env files

--- server/server.js
+++ server/server.js
@@ -6,9 +6,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const db = require('./db');
+const bcrypt = require('bcrypt');
+
 app.get("/",(req,res)=>{
     res.send("Backend server working ");
-})
+});
+
+app.get('/users', async (req,res)=>{
+    try{
+        const result = await db.query('SELECT * FROM users');
+        res.json(result.rows);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.post("/users",async (req,res)=>{
+    const { name, email, password} = req.body ; 
+    try{
+        const saltRounds = 10;
+        const hashedPassword =  await bcrypt.hash(password,saltRounds);
+        const result = await db.query('INSERT INTO users (name,email,password) VALUES ($1,$2,$3) RETURNING * ',[name,email,hashedPassword]);
+        const { password: _, ...userWithoutPassword } = result.rows[0];
+        res.status(201).json(userWithoutPassword);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send({ error: 'Failed to add user' });
+    }
+});
+
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
